8e4b560b3dff37cbf85f93edca1a0308b5520eca
Ian Riley <iandreariley@gmail.com>
2017-10-25 13:28:35 -0700

    Add basic run scripts in OTB for CMT and SiamFC.

---
+++ scripts/bscripts/run_CMT.py
@@ -0,0 +1,5 @@
+def run_CMT(seq, result_path, save_image):
+    print seq
+    print result_path
+    print save_image
+    return {'res': [], 'type': 'rect', 'fps': 1}
---
+++ scripts/bscripts/run_SiamFC.py
@@ -0,0 +1,5 @@
+def run_SiamFC(seq, results_path, save_image):
+    print seq
+    print results_path
+    print save_image
+    return {'res': [], 'type': 'rect', 'fps': 1}

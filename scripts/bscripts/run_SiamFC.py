8776be42270be72788d434d5b641780684d3d90d
Ian Riley <iandreariley@gmail.com>
2017-10-26 15:43:57 +0000

    use global siamfc detector to avoid tf error on construction.

--- scripts/bscripts/run_SiamFC.py
+++ scripts/bscripts/run_SiamFC.py
@@ -1,11 +1,37 @@
-import model.detector as detector
+import scripts.model.detector as detector
 import time
 import scipy.ndimage as ndimage
 import json
 import collections
 
 
+def parse_arguments():
+    with open('/home/ubuntu/src/siamfc-tf/parameters/hyperparams.json') as json_file:
+        hp = json.load(json_file)
+    with open('/home/ubuntu/src/siamfc-tf/parameters/evaluation.json') as json_file:
+        ev = json.load(json_file)
+    with open('/home/ubuntu/src/siamfc-tf/parameters/run.json') as json_file:
+        run = json.load(json_file)
+    with open('/home/ubuntu/src/siamfc-tf/parameters/environment.json') as json_file:
+        env = json.load(json_file)
+    with open('/home/ubuntu/src/siamfc-tf/parameters/design.json') as json_file:
+        design = json.load(json_file)
+
+    hp = collections.namedtuple('hp', hp.keys())(**hp)
+    ev = collections.namedtuple('evaluation', ev.keys())(**ev)
+    run = collections.namedtuple('run', run.keys())(**run)
+    env = collections.namedtuple('env', env.keys())(**env)
+    design = collections.namedtuple('design', design.keys())(**design)
+
+    return hp, ev, run, env, design
+
+
+hp, ev, run, env, design = parse_arguments()
+siamfc_detector = detector.SiameseNetwork(hp, design, env)
+
+
 def run_SiamFC(seq, results_path, save_image):
+    global siamfc_detector
     seq_len = seq.endFrame - seq.startFrame + 1
     results = [[0., 0., 0., 0.]] * seq_len
 
@@ -15,8 +41,7 @@ def run_SiamFC(seq, results_path, save_image):
     seq_frames = seq.s_frames[start_frame : end_frame + 1]
 
     results[0] = seq.init_rect
-
-    trkr = detector.SiameseNetwork()
+    trkr = siamfc_detector
     trkr.set_target(_read_image(seq_frames[0]), seq.init_rect)
 
     start = time.time()
@@ -32,22 +57,3 @@ def _read_image(fname):
     return ndimage.imread(fname)[:, :, ::-1]
 
 
-def parse_arguments():
-    with open('/home/ubuntu/siamfc-tf/parameters/hyperparams.json') as json_file:
-        hp = json.load(json_file)
-    with open('/home/ubuntu/siamfc-tf/parameters/evaluation.json') as json_file:
-        ev = json.load(json_file)
-    with open('/home/ubuntu/siamfc-tf/parameters/run.json') as json_file:
-        run = json.load(json_file)
-    with open('/home/ubuntu/siamfc-tf/parameters/environment.json') as json_file:
-        env = json.load(json_file)
-    with open('/home/ubuntu/siamfc-tf/parameters/design.json') as json_file:
-        design = json.load(json_file)
-
-    hp = collections.namedtuple('hp', hp.keys())(**hp)
-    ev = collections.namedtuple('evaluation', ev.keys())(**ev)
-    run = collections.namedtuple('run', run.keys())(**run)
-    env = collections.namedtuple('env', env.keys())(**env)
-    design = collections.namedtuple('design', design.keys())(**design)
-
-    return hp, ev, run, env, design
